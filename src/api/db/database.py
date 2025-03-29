9c2d5a08e36a1b33826c9e59b79698f3001e57c9
Renee Hsu <piaoyunh@gmail.com>
2025-03-29 15:17:59 +0800

    feat: add connection pool config

--- src/api/db/database.py
+++ src/api/db/database.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20, pool_timeout=30, pool_recycle=1800, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
